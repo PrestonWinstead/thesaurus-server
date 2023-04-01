1b8c5a7a7dd8afe03d28a41d498c8554ad29f9bb
Preston Winstead <prestonwinstead@me.com>
2023-03-31 22:41:07 -0500

    fix typo

--- app/controllers/antonyms_controller.rb
+++ app/controllers/antonyms_controller.rb
@@ -1,6 +1,6 @@
 class AntonymsController < ApplicationController
   def index
     puts "in antonyms controller #{params}"
-    render json: AntonymService.new(params[:search]).find_antonymss
+    render json: AntonymService.new(params[:search]).find_antonyms
   end
 end
